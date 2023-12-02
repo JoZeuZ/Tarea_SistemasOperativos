16ed0526e365a83bbf9387853a432de88419af5b
JoZeuZ <jose.rodriguez2001@alumnos.ubiobio.cl>
2023-12-02 18:45:37 -0300

    Se hacen pruebas, ya se puede pasar a escala de grises la imagen

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -1,20 +1,30 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
 int main() {
-    Mat image = imread("imagenacolor.jpg", IMREAD_COLOR);
+    // Leer la imagen a color
+    Mat colorImage = imread("imagenacolor.jpg", IMREAD_COLOR);
 
-    // read each pixel (RGB pixel)
-    for (int r = 0; r < image.rows; r++) {
-        for (int c = 0; c < image.cols; c++) {
-            Point3_<uchar>* p = image.ptr<Point3_<uchar>>(r, c);
-            // X: B, Y: G, Z: R
-            printf("(%d %d %d) ", p->x, p->y, p->z);
+    // Crear una matriz vacía del mismo tamaño para la imagen en escala de grises
+    Mat grayImage(colorImage.rows, colorImage.cols, CV_8UC1);
+
+    // Procesar cada píxel
+    for (int r = 0; r < colorImage.rows; r++) {
+        for (int c = 0; c < colorImage.cols; c++) {
+            Vec3b pixel = colorImage.at<Vec3b>(r, c);
+
+            // Convertir a escala de grises utilizando el promedio
+            uchar grayValue = (pixel[0] + pixel[1] + pixel[2]) / 3;
+
+            grayImage.at<uchar>(r, c) = grayValue;
         }
     }
+
+    // Guardar la imagen en escala de grises
+    imwrite("imagenengrises.jpg", grayImage);
 
     return 0;
 }
