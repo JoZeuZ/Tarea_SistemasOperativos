2f31ba1ac1e7d7695602e7c909ba74adf4ec5159
Cristobal Amaya <g3nericn4med@gmail.com>
2023-12-03 19:38:28 -0300

    Se comenta el código faltante

--- Paralelo/OpenMP/Paralelo_openmp.cpp
+++ Paralelo/OpenMP/Paralelo_openmp.cpp
@@ -8,23 +8,25 @@ using namespace std;
 using namespace std::chrono;
 
 int main(int argc, char** argv) {
+    // Verifica si se ingresan los argumentos correctos al ejecutar el programa
     if (argc != 4) {
         cerr << "Uso: " << argv[0] << " <imagen_entrada> <imagen_salida> <num_threads>" << endl;
         return 1;
     }
-
+    // Carga la imagen en color desde la ruta proporcionada por el primer argumento
     Mat colorImage = imread(argv[1], IMREAD_COLOR);
     if (colorImage.empty()) {
         cerr << "Error al cargar la imagen: " << argv[1] << endl;
         return 1;
     }
-
+    // Muestra información sobre las dimensiones de la imagen cargada
     cout << "Loading image . . ." << endl;
     cout << "Rows (height): " << colorImage.rows << " Cols (width): " << colorImage.cols << endl;
-
+    // Crea una imagen en escala de grises con las mismas dimensiones que la imagen original
     Mat grayImage(colorImage.rows, colorImage.cols, CV_8UC1);
+    // Obtiene el número de hilos (threads) a utilizar desde el tercer argumento
     int numThreads = stoi(argv[3]);
-
+    
     cout << "Start conversion . . ." << endl;
     auto start = high_resolution_clock::now();
 
@@ -39,9 +41,9 @@ int main(int argc, char** argv) {
 
     auto stop = high_resolution_clock::now();
     cout << "End conversion . . ." << endl;
-
+    // Guarda la imagen en escala de grises en la ruta proporcionada por el segundo argumento
     imwrite(argv[2], grayImage);
-
+    // Calcula y muestra el tiempo total empleado en la conversión
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Total time spent in seconds is " << duration.count() / 1000000.0 << endl;
 
--- Paralelo/Thread/Paralelo_thread.cpp
+++ Paralelo/Thread/Paralelo_thread.cpp
@@ -19,44 +19,45 @@ void convertToGrayscale(const Mat& input, Mat& output, int startRow, int endRow)
 }
 
 int main(int argc, char** argv) {
+    // Verifica si se ingresan los argumentos correctos al ejecutar el programa
     if (argc != 4) {
         cerr << "Uso: " << argv[0] << " <imagen_entrada> <imagen_salida> <num_threads>" << endl;
         return 1;
     }
-
+    // Carga la imagen en color desde la ruta proporcionada por el primer argumento
     Mat colorImage = imread(argv[1], IMREAD_COLOR);
     if (colorImage.empty()) {
         cerr << "Error al cargar la imagen: " << argv[1] << endl;
         return 1;
     }
-
+    // Muestra información sobre las dimensiones de la imagen cargada
     cout << "Loading image . . ." << endl;
     cout << "Rows (height): " << colorImage.rows << " Cols (width): " << colorImage.cols << endl;
-
+    // Crea una imagen en escala de grises con las mismas dimensiones que la imagen original
     Mat grayImage(colorImage.rows, colorImage.cols, CV_8UC1);
     int numThreads = stoi(argv[3]);
 
-    vector<thread> threads;
-    int rowsPerThread = colorImage.rows / numThreads;
-
+    vector<thread> threads; // Vector para almacenar los hilos
+    int rowsPerThread = colorImage.rows / numThreads; // Número de filas por hilo
+    
     cout << "Start conversion . . ." << endl;
     auto start = high_resolution_clock::now();
-
+     // Crea hilos para procesar secciones de la imagen en paralelo
     for (int i = 0; i < numThreads; i++) {
         int startRow = i * rowsPerThread;
         int endRow = (i == numThreads - 1) ? colorImage.rows : (i + 1) * rowsPerThread;
         threads.emplace_back(convertToGrayscale, cref(colorImage), ref(grayImage), startRow, endRow);
     }
-
+    // Espera a que todos los hilos completen su trabajo
     for (auto& th : threads) {
         th.join();
     }
-
+    
     auto stop = high_resolution_clock::now();
     cout << "End conversion . . ." << endl;
-
+    // Guarda la imagen en escala de grises en la ruta proporcionada por el segundo argumento
     imwrite(argv[2], grayImage);
-
+    // Calcula y muestra el tiempo total empleado en la conversión
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Total time spent in seconds is " << duration.count() / 1000000.0 << endl;
 
